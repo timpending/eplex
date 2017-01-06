311d61f658e30717f89d1cde1ec4cc673c9faabc
Tim Pender <mrtimpender@gmail.com>
2017-01-06 00:29:53 -0700

    hmtl shell built

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // Initialize collapse button
+$(document).ready(function() {
   $(".button-collapse").sideNav({
-    menuWidth: 500
+    menuWidth: 350
   });
 
+  $('select').material_select();
+});
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
   $('.menu-header').on('click', ()=>{
