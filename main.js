1ff6af93d86b598677c6bf85b3af3f7015216870
Tim Pender <mrtimpender@gmail.com>
2017-01-06 13:58:18 -0700

    form object formatted

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 // Initialize collapse button
 $(document).ready(function() {
-  $(".button-collapse").sideNav({
+  $('.button-collapse').sideNav({
     menuWidth: 350
   });
-
   $('select').material_select();
-});
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
+  $('#cloud-equiv').material_select();
+
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
   $('.menu-header').on('click', ()=>{
     $('.button-collapse').sideNav('hide')
   });
+
+// Local Storage Form
+$('#submit').on('click', ()=>{
+    let planObject = {};
+    planObject.planName = $('#plan-name').val();
+    planObject.computeBacking = $('#compute-backing').val();
+    planObject.cloudEquiv = $('#cloud-equiv').val();
+    planObject.cloudEquivSize = $('#cloud-equiv-size').val();
+    planObject.assocOrg = $('#assoc-org').val();
+    planObject.assocOrgDesc = $('#assoc-org-desc').val()
+    planObject.partnerCompute = $('#partner-compute').val();
+    planObject.partnerAddOn = $('#partner-add-on').val();
+    planObject.partnerAddOnFee = $('#partner-add-on-fee').val();
+    planObject.catalog1 = $('#catalog1')[0].checked;
+    planObject.catalog2 = $('#catalog2')[0].checked;
+    planObject.catalog3 = $('#catalog3')[0].checked;
+
+    console.log(planObject);
+
+  // ADD GOVERNANCE + Dynamicism
+  // ADD COMPUTE, other select validations (non-nulls)
+
+  });
+});
